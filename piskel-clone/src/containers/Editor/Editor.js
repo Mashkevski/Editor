78bb55cf5c14df7b78b5ae202479ce836a9b3ac4
Siarhei Mashkeuski <cybera@list.ru>
2019-06-07 10:14:04 +0300

    init: start piskel-clone

---
+++ piskel-clone/src/containers/Editor/Editor.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: 32,
+    };
+  }
+
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+export default Editor;
