a2f2b633af542484c20821ec25946422a2c96cc4
Siarhei Mashkeuski <cybera@list.ru>
2019-06-09 18:27:18 +0300

    feat: add mouse event handler on the canvas

--- piskel-clone/src/containers/Editor/Editor.js
+++ piskel-clone/src/containers/Editor/Editor.js
@@ -4,6 +4,8 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout/Layout';
 import Canvas from '../../components/Canvas/Canvas';
+import { toolActionMap, getPixelPosition } from './toolAction';
+
 
 class Editor extends Component {
   constructor(props) {
@@ -14,6 +16,13 @@ class Editor extends Component {
       secondaryColor: '#ffffff00',
       primaryColor: '#c21f1f',
       currentAction: 'drawPixel',
+      activeFrameIndex: 0,
+      frames: [],
+      pixels: new Array(16).fill('#ffffff00'),
+      startCoord: {x: 0, y: 0},
+      startPixels: [],
+      coordinatesArray: [],
+      selectedPixels: [],
     };
   }
 
@@ -35,6 +44,56 @@ class Editor extends Component {
   onColorSelect(evt) {
     const { dataset, value } = evt.target;
     this.setState({ [dataset.input]: value });
+  }
+
+
+  onMouseDown(evt) {
+    const { target } = evt;
+    const { state } = this;
+    this.setState({ startPixels: state.pixels.slice() });
+    let pixels = state.pixels.slice();
+    const { scale, currentAction } = this.state;
+    let coord = getPixelPosition(evt, scale);
+    let result = null;
+    const startCoord = {...coord};
+    let prevCoord = coord;
+    result = toolActionMap[currentAction]({ startCoord, coord }, state, target, pixels);
+    const onMove = (moveEvent) => {
+      if (moveEvent.buttons === 0) {
+        target.removeEventListener('mousemove', onMove);
+      } else {
+        coord = getPixelPosition(moveEvent, scale);
+        if (coord.x === prevCoord.x
+          && coord.y === prevCoord.y) return;
+          prevCoord = coord;
+        result = toolActionMap[currentAction]({ startCoord, coord }, state, target, pixels);
+        if (result.returnedPixels)
+          pixels = result.returnedPixels.slice();
+      }
+    };
+
+    const onMouseUp = () => {
+      this.mouseUp(pixels, result.coordinatesArray || null);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    target.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', onMouseUp);
+  }
+
+  mouseUp(pixels, coords) {
+    const { activeFrame } = this.state;
+    if (this.state.currentAction !== 'fillRectangle') {
+      this.setState({ pixels: pixels.slice() });
+    };
+    if (coords) {
+      this.setState({ coordinatesArray: coords.slice() });
+    };
+    this.setState((prevState) => {
+      const { frames } = prevState;
+      frames[activeFrame - 1] = pixels.slice();
+      return { frames: prevState.frames };
+    });
   }
 
   render() {
